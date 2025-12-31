3923961d9e893c0be589c093d1572932375899d3
FirojMiya7 <firojali723@gmail.com>
2025-12-31 15:39:04 +0545

    update

--- Basics/tuts8.py
+++ Basics/tuts8.py
@@ -3,23 +3,27 @@ mystr = "feroz is a good boy"       #hamilaii thaha nai xa ki yo sab character h
 mystreg = "Ferozisagoodboy"
 
 
-print(mystr)            #mystr print bhayo
-print(mystr[11])        #to print just g from the characters of mystr---> given index number where g stored
-print(mystr[0:5])       #To print whole word Feroz from the mystr---> where index 0 is included and index 5 is excluded
-print(len(mystr))       #To find out the total number of index in a given mystr.. 19 bhanera dinxa 
+print(mystr)                        #mystr print bhayo
+print(mystr[11])                    #to print just g from the characters of mystr---> given index number where g stored
+print(mystr[0:5])                   #To print whole word Feroz from the mystr---> where index 0 is included and index 5 is excluded
+print(len(mystr))                   #To find out the total number of index in a given mystr.. 19 bhanera dinxa 
 #incase maile 
-print(mystr[0:100])     #Yesari print garey bhani index 19 samma bhako print gardinxa baaki extra index laii chaii error bhanera show gardaina
-print(mystr[0:5:2])     #yesma chaii 0 dekhi 5 index samma ko character ma euta euta skip gardai print garxa Feroz-->F r z where [0:5:2] 0-initial index, 5-destinationIndex, 2reduction index where n-1 is applied if 2 lekhiyeko xa bhani 2-1>1 skip 3-1>2 skip
+print(mystr[0:100])                 #Yesari print garey bhani index 19 samma bhako print gardinxa baaki extra index laii chaii error bhanera show gardaina
+print(mystr[0:5:2])                 #yesma chaii 0 dekhi 5 index samma ko character ma euta euta skip gardai print garxa Feroz-->F r z where [0:5:2] 0-initial index, 5-destinationIndex, 2reduction index where n-1 is applied if 2 lekhiyeko xa bhani 2-1>1 skip 3-1>2 skip
 #incase [:]-->suru ko ma 0 initial aafai linxa, second ko ma all index linxa automatic again inCase[::]-->suru ko ma 0 initial aafai linxa, second ko ma all index linxa automatic,and in the last reduction wala ma chai be Default 1 hunxa so yesley aafai 1 linxa.
-print(mystr[::])        #Act neutral
+print(mystr[::])                    #Act neutral
 
 
 
-print(mystr.isalnum())     #->False     Boolean 
-print(mystreg.isalnum())    #->True     alphanumeric value like email where gaps are not present
+print(mystr.isalnum())              #->False     Boolean 
+print(mystreg.isalnum())            #->True     alphanumeric value like email where gaps are not present
 
-print(mystr.isalpha())      #->False
+print(mystr.isalpha())              #->False
 print(mystr.endswith("boy"))        #->True also case sensitive if "Boy" was given then it will be false
 print(mystr.count("o"))             #->There are 4 o present in string mystr.
 print(mystr.capitalize())           #->First letter lai capital gardinxa if small ma xa bhani.
+print(mystr.lower())                #->Whole sentence lai naii lower ma convert gardinxa ani
+print(mystr.upper())                #->whole sentence laii upper case ma convert gardinxa.
 print(mystr.find("good"))           #->index 11 dekhi start bhanera dekhaunxa so yesari find garna sajilo hunxa.
+
+print(mystr.replace("is" , "are"))  #->yesari hamile whole word or letter lai nai replace garna sakinxa
