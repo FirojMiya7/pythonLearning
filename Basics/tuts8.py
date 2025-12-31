db6d016c6459f29785a20393373e5a01038f2129
FirojMiya7 <firojali723@gmail.com>
2025-12-31 15:17:17 +0545

    update

--- Basics/tuts8.py
+++ Basics/tuts8.py
@@ -8,3 +8,5 @@ print(len(mystr))       #To find out the total number of index in a given mystr.
 #incase maile 
 print(mystr[0:100])     #Yesari print garey bhani index 19 samma bhako print gardinxa baaki extra index laii chaii error bhanera show gardaina
 print(mystr[0:5:2])     #yesma chaii 0 dekhi 5 index samma ko character ma euta euta skip gardai print garxa Feroz-->F r z where [0:5:2] 0-initial index, 5-destinationIndex, 2reduction index where n-1 is applied if 2 lekhiyeko xa bhani 2-1>1 skip 3-1>2 skip
+#incase [:]-->suru ko ma 0 initial aafai linxa, second ko ma all index linxa automatic again inCase[::]-->suru ko ma 0 initial aafai linxa, second ko ma all index linxa automatic,and in the last reduction wala ma chai be Default 1 hunxa so yesley aafai 1 linxa.
+print(mystr[::])        #Act neutral
