fad38d35aa7c53e2f557417b41532786d1361663
FirojMiya7 <firojali723@gmail.com>
2025-12-31 14:54:15 +0545

    update

---
+++ Basics/tuts8.py
@@ -0,0 +1,6 @@
+mystr = "Feroz is a good boy"       #hamilaii thaha nai xa ki yo sab character haru index ma kaam garxa
+#maile yettikai run garey bhani k e pani output aaudaina  but after adding print 
+
+print(mystr)            #mystr print bhayo
+print(mystr[11])        #to print just g from the characters of mystr---> given index number where g stored
+print(mystr[0:5])       #To print whole word Feroz from the mystr---> where index 0 is included and index 5 is excluded
