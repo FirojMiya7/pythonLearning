f070b246cd6090a7951827288af7a6d05710309f
FirojMiya7 <firojali723@gmail.com>
2025-12-31 12:26:17 +0545

    update

---
+++ Basics/tuts.py
@@ -0,0 +1,21 @@
+"""
+This 
+is 
+a multi
+line comment
+"""
+
+""" This is a multi line comment in a single statement """
+
+
+
+# print("Hello world paxi end ma chaii space bhanna khojeko ho", end=" ")
+# print("line break hudaina and then again in jr.")
+# print("next line")
+
+
+# print("Escape")
+#
+# print("C:\\Feroz")
+
+#comment before statement  (which cause whole statement)   print("Feroz is \n good boy \t isn't is?")  #Comment after statement
---
+++ Basics/tuts7.py
@@ -0,0 +1,13 @@
+#Variable
+
+var1 = "Hello World"
+var2 = 4
+var3 = 3.14
+
+print(var1)
+print(var2)
+print(var3)
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
