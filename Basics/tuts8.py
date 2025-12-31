fab8c2de7e3445efaf986202fd13f6f299d4a9a6
FirojMiya7 <firojali723@gmail.com>
2025-12-31 14:59:09 +0545

    update

--- Basics/tuts8.py
+++ Basics/tuts8.py
@@ -4,3 +4,4 @@ mystr = "Feroz is a good boy"       #hamilaii thaha nai xa ki yo sab character h
 print(mystr)            #mystr print bhayo
 print(mystr[11])        #to print just g from the characters of mystr---> given index number where g stored
 print(mystr[0:5])       #To print whole word Feroz from the mystr---> where index 0 is included and index 5 is excluded
+print(len(mystr))       #To find out the total number of index in a given mystr..
