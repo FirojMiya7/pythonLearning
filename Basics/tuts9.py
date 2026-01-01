0f362e0479d3c34bc1c4d71358e92c8b2588a140
FirojMiya7 <firojali723@gmail.com>
2026-01-01 20:35:47 +0545

    update

--- Basics/tuts9.py
+++ Basics/tuts9.py
@@ -5,7 +5,11 @@ grocery = ["Harpic", "Vim bar", "deodrant", "Bhindi", "Lollipop",56]
 
 print(grocery)              #whole grocery nai print garxa
 print(grocery[3])           #Index number 3 ko value print hunxa --> Bhindi
+
+#Slicing 
 print(grocery[0:4])         #Index number 0 dekhi 3 samma ko print garxa cus 4 excluded hunxa or n-1 ma kaam garxa
+print(grocery[2:5])         #Index number 2 dekhi 4 ko samma ko linxa ani dekhaunxa
+
 #print(grocery[6])          #Index number 6 is not present so error occurs
 
 
