1ee516cbf213c46f89b14ad1dfaa1ae7778eeb79
FirojMiya7 <firojali723@gmail.com>
2026-01-02 22:16:25 +0545

    update

--- Basics/tuts10.py
+++ Basics/tuts10.py
@@ -1,7 +1,7 @@
 #Dictionary is nothing but key value pairs
 
-L1 = []         #Yo bracket le determine garxa so yo ho list
-T2 = ()         #Yo bracket le determine garxa so yo ho tupple
+L1 = []                 #Yo bracket le determine garxa so yo ho list
+T2 = ()                 #Yo bracket le determine garxa so yo ho tupple
 
-d3 = {}         #Yo bracket le determine garxa so yo ho dictionary
-print
+d1 = {}                 #Yo bracket le determine garxa so yo ho dictionary
+print(type(d1))         #<class 'dict'>
