c0fcc0bd336676c80b07147e084326047e2913cc
FirojMiya7 <firojali723@gmail.com>
2026-01-02 22:15:02 +0545

    update

---
+++ Basics/tuts10.py
@@ -0,0 +1,7 @@
+#Dictionary is nothing but key value pairs
+
+L1 = []         #Yo bracket le determine garxa so yo ho list
+T2 = ()         #Yo bracket le determine garxa so yo ho tupple
+
+d3 = {}         #Yo bracket le determine garxa so yo ho dictionary
+print
