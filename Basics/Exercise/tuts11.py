004d1ba2e57016b518d54b23f7051c14faf37ecc
FirojMiya7 <firojali723@gmail.com>
2026-01-03 10:18:22 +0545

    update

--- Basics/Exercise/tuts11.py
+++ Basics/Exercise/tuts11.py
@@ -1,3 +1,7 @@
 """
 Create a dictionary and take user input and return the meaning of the word from the dictionary
+
+Literally euta dictionary joo banaune where i can search the meaning of word
 """
+
+d1 = 
--- Basics/Learning/tuts10.py
+++ Basics/Learning/tuts10.py
@@ -7,7 +7,8 @@ d1 = {}                 #Yo bracket le determine garxa so yo ho dictionary
 print(type(d1))         #<class 'dict'> yesari output aaunxa dictionary ho dekhaunxa.
 
 
-d2 = {"Feroz":"Pizza", "Sushil":"Burger",
+d2 = {"Feroz":"Pizza", 
+      "Sushil":"Burger",
       "Samyam":"Croissant",
       "Trishan":"Dhedo",
       "Suhan":{"B":"Roti",                      #where B=Breakfast , L=lunch, D=Dinner.
@@ -25,7 +26,7 @@ print("Feroz eat:", d2["Feroz"])
 print("\n",d2)
 #print(d2["Sushil","Feroz"])         #yedi duita ko chaiyo bhani yesari wrong hunxa error show garxa baru instead aarko use gara method.
 
-#Multiple from dictionary
+#To print Multiple from dictionary
 print("\nFeroz eat:", d2["Feroz"],
       "\nSushil eat:", d2["Sushil"],
       "\nSamyam eat:", d2["Samyam"], 
