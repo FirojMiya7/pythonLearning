58daaa413b3d244334b1ac0769f1c368664f064f
FirojMiya7 <firojali723@gmail.com>
2026-01-04 18:11:06 +0545

    update

--- Basics/Learning/tuts13.py
+++ Basics/Learning/tuts13.py
@@ -12,4 +12,10 @@ if var3>var2:
     print(f"{var3} is greater than {var2}")             #yaha maile suru ma f lekheko xu kinaki hamle c ma code garda %d garnu parthyo testaii yesma format milauna
 else:
     print(f"{var3} is less than {var2}")                #python ma chaii indentation error aaunxa matlab yesma harek code haru laii form ma lekhnu parney hunxa that means print ko aagadi tab space xa nii testaii tyo sab check garnu parxa if maile without tab space print func lekhthiye bhani tyo error aaunthyo
-    
+
+
+"""
+for example maile indentation ko laagi
+print("{var3} is less than {var2}")    ----> yesma print bhanda aagadi tab space theana so indentation error show garxa 
+        yesto loops soops haru ko laagi
+"""
