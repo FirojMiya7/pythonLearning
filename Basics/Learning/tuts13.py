d4a96f415a92bb5051cee873b2bcf4d46d346335
FirojMiya7 <firojali723@gmail.com>
2026-01-04 18:03:12 +0545

    update

---
+++ Basics/Learning/tuts13.py
@@ -0,0 +1,9 @@
+var1 = 26
+var2 = 54
+
+var3 = int(input("Enter any Number:"))
+
+if var3>var2:
+    print(f"{var3} is greater than {var2}")             #yaha maile suru ma f lekheko xu kinaki hamle c ma code garda %d garnu parthyo testaii yesma format milauna
+else:
+    print(f"{var3} is less than {var2}")
