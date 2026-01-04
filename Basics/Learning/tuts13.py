f17c1ac802024f64c42796ae5f2728e9e388287b
FirojMiya7 <firojali723@gmail.com>
2026-01-04 18:07:10 +0545

    update

--- Basics/Learning/tuts13.py
+++ Basics/Learning/tuts13.py
@@ -11,4 +11,5 @@ var3 = int(input("Enter any Number:"))
 if var3>var2:
     print(f"{var3} is greater than {var2}")             #yaha maile suru ma f lekheko xu kinaki hamle c ma code garda %d garnu parthyo testaii yesma format milauna
 else:
-    print(f"{var3} is less than {var2}")
+    print(f"{var3} is less than {var2}")                #python ma chaii indentation error aaunxa matlab yesma harek code haru laii form ma lekhnu parney hunxa that means print ko aagadi tab space xa nii testaii tyo sab check garnu parxa if maile without tab space print func lekhthiye bhani tyo error aaunthyo
+    
