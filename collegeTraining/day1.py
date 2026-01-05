3cc50ab81c3baef52dc41cae588c521f3dd3cb08
FirojMiya7 <firojali723@gmail.com>
2026-01-05 13:15:37 +0545

    update

--- collegeTraining/day1.py
+++ collegeTraining/day1.py
@@ -1,6 +1,8 @@
+#Variable
+"""
 a = 34
 print(f"In variable a there is {a} stored")
-print(type(a))
+print(type(a))"""
 
 
 
@@ -19,7 +21,7 @@ while i<7:
 
 
 #Function
-
+"""
 def show():
     print("yo function ho hai")
 
@@ -30,4 +32,5 @@ def fact():
     print("Hello")
     return 2            #fucntion exit garney kaam garxa where python is interpreter -> line by line execution.
     print("LCC")        #aaba yo print naii hudaina cuz return le exit garera function bahira lagyo and yo line execute hunai payena
-print(fact())
+print(fact())"""
+
