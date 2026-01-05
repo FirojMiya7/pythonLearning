86dc0b61839aefdec2aafac5a5842e04bf5e7adb
FirojMiya7 <firojali723@gmail.com>
2026-01-05 12:23:45 +0545

    update

--- collegeTraining/day1.py
+++ collegeTraining/day1.py
@@ -1,3 +1,11 @@
 a = 34
 print(f"In variable a there is {a} stored")
 print(type(a))
+
+nano ~/.bashrc
+
+alias ga = 'git add .'
+alias gs = 'git status'
+alias gcm = 'git commit -m'
+alias gp = 'git push'
+
