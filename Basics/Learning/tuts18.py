e79925695b23361fa8c0caf690e37d06bb816859
FirojMiya7 <firojali723@gmail.com>
2026-01-07 20:01:47 +0545

    update

--- Basics/Learning/tuts18.py
+++ Basics/Learning/tuts18.py
@@ -1,13 +1,14 @@
-#Break and Continue
+#Break and Continue in loop
+
 
 """i=0
 while(i<10):
     print(i, end=" ")               #end=" " space diney or \t ko jasto kaam garxa --> output-> 0 1 2 3 4 5 6 7 8 9 
     i=i+1"""
 
-# Break 
+# Break and continue
 
-i=0
+"""i=0
 while(True):                                #only True use garera print garney ho bhane infinite hunxa
     print(i+1, end=" ")
 
@@ -17,4 +18,8 @@ while(True):                                #only True use garera print garney h
 
     if(i==9):
         break                       # stop the loop   yaha aayera if i==9 then break and exit 
-    i = i+1
+    i = i+1"""
+
+
+#Quiz
+
