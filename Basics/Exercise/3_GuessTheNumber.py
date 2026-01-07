9d7dda61077e4b076b23d4fad2e9c878acc1f651
FirojMiya7 <firojali723@gmail.com>
2026-01-07 20:39:40 +0545

    update

---
+++ Basics/Exercise/3_GuessTheNumber.py
@@ -0,0 +1,10 @@
+# Guess The Number
+#Like Akinator
+
+n=18                    #My original number value which is hidden for user to guess...
+
+#Take user input
+#number of guesses --> 9
+#print number of guesses left
+#print number of guesses he took to finish
+#Game over
