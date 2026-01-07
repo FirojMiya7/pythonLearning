0801e6948d8462bb99dca6c3cbd74e95110ac0f8
FirojMiya7 <firojali723@gmail.com>
2026-01-07 20:40:46 +0545

    update

--- Basics/Exercise/3_GuessTheNumber.py
+++ Basics/Exercise/3_GuessTheNumber.py
@@ -4,7 +4,13 @@
 n=18                    #My original number value which is hidden for user to guess...
 
 #Take user input
+#give hints like its smaller its bigger bla bla
 #number of guesses --> 9
 #print number of guesses left
 #print number of guesses he took to finish
 #Game over
+
+
+
+
+
