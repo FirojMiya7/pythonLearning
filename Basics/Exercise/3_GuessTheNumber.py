f093f44e6459f7f4fca689eb96004552eb56d833
FirojMiya7 <firojali723@gmail.com>
2026-01-07 20:48:34 +0545

    update

--- Basics/Exercise/3_GuessTheNumber.py
+++ Basics/Exercise/3_GuessTheNumber.py
@@ -10,7 +10,23 @@ n=18                    #My original number value which is hidden for user to gu
 #print number of guesses he took to finish
 #Game over
 
+guesses=1
+max-guesses=9
 
+inp=int(input("\nGuess the number: "))
 
-
-
+while(guesses!=max-guesses):
+    if inp<n:
+        print("\nYour guess is smaller than the number")
+        guesses = guesses + 1
+        print(f"\nYou have {max-guesses - guesses} guesses left")
+        inp = int(input("\nGuess the number: "))
+    elif inp>n:
+        print("\nYour guess is greater than the number")
+        guesses = guesses + 1
+        print(f"\nYou have {max-guesses - guesses} guesses left")
+        inp = int(input("\nGuess the number: "))
+    else:
+        print(f"\nYou guessed it right in {guesses} guesses")
+        break
+    
