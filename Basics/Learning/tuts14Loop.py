ee934c47c91d97b06d81a2f629f60e2a69ee3443
FirojMiya7 <firojali723@gmail.com>
2026-01-16 22:08:27 +0545

    update

--- Basics/Learning/tuts14Loop.py
+++ Basics/Learning/tuts14Loop.py
@@ -59,6 +59,21 @@ items=["Feroz",34,45,"354",2,3,455,6,67,"7"]
 for item in items:
     if str(item).isnumeric() and int(item) > 6:                 #item lai str ma convert garxa  ani numeric value ho ki nai check garxa if ho bhani again int ma convert garera compare garxa ani print output
         print(item)
+
+#Instead we can do
+
+items=["Feroz",34,45,"354",2,3,455,6,67,"7",6.5]
+
+found = False
+for item in items:
+    if type(item)==int or type(item)==float:                            #yesari chai output error aaunxa haala ki program right naii xa..
+        if(item>=6):
+            print(item)
+            found = True
+
+if not found:
+    print("There is no number greater than 6")
+    
 """
 
 
