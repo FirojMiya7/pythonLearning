c914ad10a337e0403bd434bdde4c143104abdc6a
FirojMiya7 <firojali723@gmail.com>
2026-01-18 22:44:22 +0545

    update

--- Basics/Learning/tuts19.py
+++ Basics/Learning/tuts19.py
@@ -18,9 +18,22 @@ for x in num:
     print(x)"""
 
 
+
 #aahile samma tw input ligda string ma lirathyo aaba maile integer ma lina paryo bhani k 
 # garney kasari change garney?
 
+
+
+
+# Method 1 - int() then str() (complicated)
+
+num = int(input("Enter any number: "))
+num = str(num)
+
+index = 0
+while index < len(num):
+    print(f"{index+1}:",num[index])
+    index += 1
 
 
 # Method 2 - int() then str() (complicated)
