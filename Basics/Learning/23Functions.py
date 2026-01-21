37890a8542ec478f8546f1fa6bf796af26b22782
FirojMiya7 <firojali723@gmail.com>
2026-01-21 22:24:32 +0545

    update

--- Basics/Learning/23Functions.py
+++ Basics/Learning/23Functions.py
@@ -4,7 +4,7 @@
 """
 a = 9
 b = 6
-c = sum(( a, b))                #Yo ho built in function ctrl + leftClick garesi khulxa
+c = sum((a, b))                #Yo ho built in function ctrl + leftClick garesi khulxa
 
 print(c)                        #---> 15 ans aaunxa."""
 
