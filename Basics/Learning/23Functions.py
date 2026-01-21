6b2595888fb6a768bb0e1363daf4d3be31830db9
FirojMiya7 <firojali723@gmail.com>
2026-01-21 21:06:49 +0545

    update

---
+++ Basics/Learning/23Functions.py
@@ -0,0 +1,9 @@
+# Function
+
+# Example of Built In function
+"""
+a = 9
+b = 6
+c = sum(( a, b))                #Yo ho built in function ctrl + leftClick garesi khulxa
+
+print(c)                        #---> 15 ans aaunxa."""
