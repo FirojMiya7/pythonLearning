29b4282e9722a0851ae5aa909f36edf70e6ef390
FirojMiya7 <firojali723@gmail.com>
2026-01-21 22:43:29 +0545

    update

--- Basics/Learning/23Functions.py
+++ Basics/Learning/23Functions.py
@@ -46,10 +46,17 @@ function3(a, b)                    # yo chai user input ko lagi ho"""
 #Function with return statement
 """
 def function4(a, b):
+
+    # """"""This function is for calculating average of two numbers""""""            #yesari double quete use garera docstring banaucha
+
     average = (a+b)/2
     print(f"The average of {a} and {b} is", average)
-    return average              # return le value farkaunxa jaslai variable ma store garna sakinxa
+    return average              # return le value farkaunxa jaslai variable ma store garna sakinxa if return nabhaye paar print garda none garxa
 
 avg = function4(4, 7)           # avg variable ma return value store gareko
 
-print(avg)                      # avg variable print gareko"""
+print(avg)                      # avg variable print gareko
+
+# print(function4.__doc__)                     # yo chai function ko docstring print garxa
+
+# docstring chai kina lekhinxa bhane function ko barema aru le bujhna sajilo hos bhanera ho means documentation ko lagi ho"""
