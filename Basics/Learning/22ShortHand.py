a4cbb5ca0c749146b9ce6aedef68198c05487ec9
FirojMiya7 <firojali723@gmail.com>
2026-01-21 21:01:22 +0545

    update

---
+++ Basics/Learning/22ShortHand.py
@@ -0,0 +1,10 @@
+# yo k e haina just one liner code for example
+
+
+"""
+a = int(input("Enter a number: "))
+b = int(input("Enter another number: "))
+
+# if a>b: print(f"{a} is greater than {b}")
+
+print(f"{a} is greater than {b}") if a>b else print(f"{b} is greater than {a}")"""
