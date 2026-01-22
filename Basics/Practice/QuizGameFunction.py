b5af812b5b12fde53e96591171d0d5a1d7df354f
FirojMiya7 <firojali723@gmail.com>
2026-01-22 20:47:23 +0545

    update

---
+++ Basics/Practice/QuizGameFunction.py
@@ -0,0 +1,56 @@
+#Simple Quiz Game using Functions (Practice)
+
+#Requirements:
+"""
+1. Minimum 3 Questions define garney (question + correct answer)
+2. askQuestion function:
+    a.Question user lai sodhne.
+    b.User ko answer correct cha bhane 1 point add garney , wrong bhaye 0 point add garney.
+3. showScore function: Total score print garney
+4. User le case insensitive answer haley pani thik answer ma count garney
+"""
+
+
+#Goal:
+"""
+Functions practice (askQuestion , showScore)
+Conditonal logic (correct / incorrect check garney) practice.
+"""
+
+
+#Implementation:
+
+def askQuestion():
+    score = 0
+    
+    ans = input("What is the capital of Nepal: ")
+    if ans.strip().lower() == "kathmandu":
+        print("Correct Answer")
+        score += 1
+    else:
+        print("Incorrect Answer")
+    
+    try:
+        ans2 = int(input("\n5 + 2 = "))
+        if ans2 == 7:
+            print("Correct Answer")
+            score += 1
+        else:
+            print("Incorrect Answer")
+    except ValueError:
+        print("Please Enter Integer value.")
+        
+    ans3 = input("\nWhat is the largest planet in our solar system? ")
+    if ans3.strip().lower() == "jupiter":
+        print("Correct Answer")
+        score += 1
+    else:
+        print("Incorrect Answer")
+    
+    return score
+
+def showScore(totalScore):
+    print("\nYour total score is:", totalScore)
+
+totalScore = askQuestion()
+showScore(totalScore)
