507c19cc24df9d67ea9449446b94168bd1ee5ecf
FirojMiya7 <firojali723@gmail.com>
2026-01-22 09:46:50 +0545

    update

--- Basics/Practice/BankSystemUsingFunction.py
+++ Basics/Practice/BankSystemUsingFunction.py
@@ -27,33 +27,34 @@ def deposit(balance,amount):
 def withdraw(balance, amount):
     """Function to withdraw money from the account"""
     if amount > balance:
-        print("Insufficient Balance")
+        print("Insufficient Balance\n")
         return balance
+    print("Successfully withdrew!\n")
     return balance - amount
     
 def checkBalance(balance):
     """Function to check the current balance"""
-    print(f"Your current balance is: {balance}")        #or print("your current balance is:" , balance)
+    print(f"Your current balance is: {balance}\n")        #or print("your current balance is:" , balance)
 
-balance = 0
-
-choice = int(input("Please choose:\n1. For Deposit\n2. For Withdraw\n3. For CheckBalance\n4. For Exit"))
+balance = 0              #Initial Balance set gareko
 
 while True:
+    choice = int(input("\n-----Welcome to Simple Bank System-----\n\nPlease choose:\n1. For Deposit\n2. For Withdraw\n3. For CheckBalance\n4. For Exit\n\nEnter your choice: "))
+    
     if choice == 1:
-        amt = int(input("Enter your amount to deposit:"))
+        amt = int(input("Enter your amount to deposit: "))
         balance = deposit(balance, amt)
+        print(f"Successfully deposited!\n")
     elif choice == 2:
-        amt = int(input("Enter your amount for withdraw:"))
+        amt = int(input("Enter your amount for withdraw: "))
         balance = withdraw(balance, amt)
     elif choice == 3:
-        balance = checkBalance(balance)
+        checkBalance(balance)
     elif choice == 4:
         print("Thank you for Choosing Us.")
         break
     else:
-        print("Invalid Option. Please try again.")
-    # choice = input("Please choose:\n1. For Deposit\n2. For Withdraw\n3. For CheckBalance\n4. For Exit")
+        print("Invalid Option. Please try again.\n\n")
 
 
 
