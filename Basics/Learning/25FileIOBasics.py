5acd9fb89949b704d533eb89b642e9ba9f006f68
FirojMiya7 <firojali723@gmail.com>
2026-01-27 12:00:37 +0545

    update

---
+++ Basics/Learning/25FileIOBasics.py
@@ -0,0 +1,20 @@
+#File IO basics in Python
+"""
+"r" - Read file for reading only
+"w" - Write file for writing only (overwrites existing file)
+"a" - Append file for writing only (adds to the end of the file)
+"x" - Create file_(fails if file exists)
+"b" - Binary mode
+"t" - Text mode (default)
+"+" - Read and write
+"""
+
+#Question of the day:
+
+# How to show the docstring of a Python file?
+
+print("docstring:", __doc__)
+
+# print(function4.__doc__)                  #To show the docstring of a function
+# print(Class1.__doc__)                     #To show the docstring of a class
+# print(Class1.method1.__doc__)            #To show the docstring of a method inside a class
---
+++ Basics/Learning/26FileWriting.py
@@ -0,0 +1,4 @@
+f = open("Basics/Learning/Feroz.txt", "r")
+content = f.read()
+print(content)
+f.close()
