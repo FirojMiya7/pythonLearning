00359ed5b26f6bb7c263def30c55b3c1810376dd
FirojMiya7 <firojali723@gmail.com>
2026-01-28 22:26:45 +0545

    update

--- Basics/Learning/File/28FileWrite.py
+++ Basics/Learning/File/28FileWrite.py
@@ -3,7 +3,7 @@
 # If the file exists, its content will be erased
 # Writing data to the file
 """
-f = open("Basics/Learning/FirojMiya.txt", "w")
+f = open("Basics/Learning/File/FirojMiya.txt", "w")
 f.write("My name is Firoj Miya\n")
 f.write("I am learning Python\n")
 f.write("I love to code\n")
@@ -14,7 +14,7 @@ print("File written successfully.\n")
 
 # Add data to the existing file using append mode
 """
-f = open("Basics/Learning/FirojMiya.txt", "a")
+f = open("Basics/Learning/File/FirojMiya.txt", "a")
 a = f.write("I am excited to learn more about file handling in Python.\n")            #jati choti run garxam teti choti add hunxa
 
 print(a)  # returns number of characters added | kati characters add bhayo bhanera
@@ -27,7 +27,7 @@ print("Data appended successfully.\n")
 
 # Handle with both reading and writing mode.
 """
-f = open("Basics/Learning/FirojMiya.txt", "r+")
+f = open("Basics/Learning/File/FirojMiya.txt", "r+")
 print(f.read())                       # Read the existing content
 f.write("Adding a new line at the end of the file.\n")   # Write new content
 f.close()
@@ -40,7 +40,7 @@ print("File read and write operations completed successfully.\n")
 # To read the file, we need to open it in read mode
 # Read the Final file content
 """
-f = open("Basics/Learning/FirojMiya.txt", "r")
+f = open("Basics/Learning/File/FirojMiya.txt", "r")
 print(f.read())
 f.close()
 """
