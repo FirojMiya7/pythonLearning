677e201dfa90fdf3ed6aefd189c25a36a9d6cede
FirojMiya7 <firojali723@gmail.com>
2026-02-03 00:14:54 +0545

    update

---
+++ Basics/Learning/36LamdaOrAnonymousFunc.py
@@ -0,0 +1,17 @@
+# Lambda or Anonymous Functions in Python
+
+#Regular function definition
+"""
+def add(x, y):
+    return x + y
+
+print("Regular Function:", add(5, 3))
+"""
+
+
+#Lambda function definition
+"""
+add = lambda x, y: x + y                # Similar to regular function but more concise(explain something in a short clear way)  (single expression)
+
+print("Lambda Function:", add(5, 3))
+"""
