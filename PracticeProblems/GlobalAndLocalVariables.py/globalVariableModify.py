e04503bb836b8b2ef19508891069dfed28f829ed
FirojMiya7 <firojali723@gmail.com>
2026-02-04 16:44:43 +0545

    update

--- PracticeProblems/GlobalAndLocalVariables.py/globalVariableModify.py
+++ PracticeProblems/GlobalAndLocalVariables.py/globalVariableModify.py
@@ -4,6 +4,21 @@ Create global variable count = 0
 Function should increase count
 """
 
+count = 0           # yo global variable ho.
+
+def Counter():
+    global count            # tells Python we want to use the global variable
+    count += 1
+    if count == 1:
+        print(count, "time called.")
+    else:
+        print(count , "times called.")
+
+Counter()
+Counter()
+Counter()                   # jati choti function call hunxa teti choti modify hunxa
+
+
 
 
 
