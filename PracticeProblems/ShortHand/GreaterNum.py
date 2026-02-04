c6afe9b693fb4361d68e35e85d77e6a780a55a5a
FirojMiya7 <firojali723@gmail.com>
2026-02-04 15:00:11 +0545

    update

---
+++ PracticeProblems/ShortHand/GreaterNum.py
@@ -0,0 +1,6 @@
+# To check greater number using shorthand if-else
+
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+
+print(f"{num1} is greater than {num2}." if num1 > num2 else f"{num2} is greater than {num1}")
---
+++ PracticeProblems/ShortHand/OddOrEven.py
@@ -0,0 +1,5 @@
+# To check if a number is odd or even using shorthand if-else
+
+num = int(input("Enter a number: "))
+
+print(f"{num} is even." if num % 2 == 0 else f"{num} is odd.")
