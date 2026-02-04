c5ba8c3d71de8f927cb3b4c6fdf26927889c561b
FirojMiya7 <firojali723@gmail.com>
2026-02-04 16:33:31 +0545

    update

---
+++ PracticeProblems/GlobalAndLocalVariables.py/globalVariableModify.py
@@ -0,0 +1,28 @@
+# Global Variable Modify
+"""
+Create global variable count = 0
+Function should increase count
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Just checking after a long time...
+
+"""
+var = 32
+var1 = "Feroz"
+
+print(type(var), type(var1))
+"""
