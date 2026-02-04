7792ca8cb40eb92e3f5b9f8842682b8478964bd2
FirojMiya7 <firojali723@gmail.com>
2026-02-04 16:00:21 +0545

    update

--- PracticeProblems/TryExceptHandling/SafeDivision.py
+++ PracticeProblems/TryExceptHandling/SafeDivision.py
@@ -6,3 +6,19 @@ ZeroDivisionError
 ValueError
 """
 
+def SafeDivision():
+    try:
+        num1 = float(input("Enter Numerator: "))
+        num2 = float(input("Enter Denominator: "))
+
+        result = num1 / num2
+        print(f"Division of Numerator: {num1} and Denominator: {num2} is",result)
+
+    except ZeroDivisionError:
+        print("Error: Can not divide by zero!.")
+        
+    except ValueError:
+        print("Error: Invalid input! Please enter numbers only.")
+
+
+SafeDivision()
