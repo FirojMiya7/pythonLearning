7f5e00f4e394d660bc8ea76254459e3801fb67fb
FirojMiya7 <firojali723@gmail.com>
2026-02-06 12:40:32 +0545

    update

---
+++ PracticeProblem/TopicProblems/Modules/CustomModule/custom.py
@@ -0,0 +1,17 @@
+# . Custom Module
+# Create myMath.py
+# Import and use functions
+
+
+import mathFeroz
+
+a = 9
+b = 2
+
+print("add:", mathFeroz.add(a, b))
+print("sub:", mathFeroz.sub(a, b))
+print("div:", mathFeroz.div(a, b))
+print("mul:", mathFeroz.mul(a, b))
+print("square:", mathFeroz.square(a))
+print("sqrt:", mathFeroz.sqrt(a))
+print("pow:", mathFeroz.pow(a, b))
---
+++ PracticeProblem/TopicProblems/Modules/CustomModule/mathFeroz.py
@@ -0,0 +1,20 @@
+def add(a,b):
+    return a + b
+
+def sub(a,b):
+    return a - b
+
+def div(a,b):
+    return a / b
+
+def mul(a,b):
+    return a * b
+
+def square(a):
+    return a * a
+
+def sqrt(a):
+    return a ** (1/2)
+
+def pow(a,i):
+    return a ** i
