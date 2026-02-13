34c356721a236cd89044e4d85b39a141cf0449ad
FirojMiya7 <firojali723@gmail.com>
2026-02-13 19:52:08 +0545

    update

---
+++ PracticeProblem/MiniProjects/tkinterForGUI/Basic/test.py
@@ -0,0 +1,47 @@
+import tkinter as tk
+from tkinter import messagebox
+
+def add_student_gui():
+    try:
+        name = name_entry.get()
+        roll = int(roll_entry.get())
+        marks = float(marks_entry.get())
+
+        with open("Students.txt", "a") as file:
+            file.write(f"{name},{roll},{marks}\n")
+
+        messagebox.showinfo("Success", "Student added successfully")
+
+        name_entry.delete(0, tk.END)
+        roll_entry.delete(0, tk.END)
+        marks_entry.delete(0, tk.END)
+
+    except ValueError:
+        messagebox.showerror("Error", "Invalid input")
+
+root = tk.Tk()
+root.title("Student Management System")
+root.geometry("200x250")
+root.label = tk.Label(root, text="Add Student", font=("Arial", 14), bg="skyblue")
+root.label.pack(pady=10)
+root.configure(bg="skyblue")
+
+tk.Label(root, text="Student Name").pack()
+name_entry = tk.Entry(root)
+name_entry.pack()
+
+tk.Label(root, text="Roll Number").pack()
+roll_entry = tk.Entry(root)
+roll_entry.pack()
+
+tk.Label(root, text="Marks").pack()
+marks_entry = tk.Entry(root)
+marks_entry.pack()
+
+tk.Button(root, text="Add Student", command=add_student_gui).pack(pady=10)
+
+root.mainloop()
+
+
+
+# Baaki paxi garney aahile laii yetti nai dhanyabad.
