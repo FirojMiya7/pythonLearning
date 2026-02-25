39156e3fcdde4c1abb3adf04045f608a47e598c0
FirojMiya7 <firojali723@gmail.com>
2026-02-25 18:22:42 +0545

    update

---
+++ PracticeProblem/Project/AutomationScript/FileOrganizer/FileOrganizer.py
@@ -0,0 +1,9 @@
+# Project 6: Automation Script (Real Power 🔥)
+"""
+Option A: File Organizer
+Folder bhitra ko files
+type anusar move:
+.jpg → Images
+.pdf → Docs
+.txt → Texts
+"""
---
+++ PracticeProblem/Project/AutomationScript/PasswordGenerator/PasswordGenerator.py
@@ -0,0 +1,7 @@
+# Project 6: Automation Script (Real Power 🔥)
+"""
+Option B: Password Generator
+Random password generate
+length user input
+save to file
+"""
