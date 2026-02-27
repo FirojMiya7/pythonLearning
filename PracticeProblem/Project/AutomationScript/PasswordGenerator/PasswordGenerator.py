f904847cf07c8751779bda51005d8f8550317269
FirojMiya7 <firojali723@gmail.com>
2026-02-27 17:26:44 +0545

    update

--- PracticeProblem/Project/AutomationScript/PasswordGenerator/PasswordGenerator.py
+++ PracticeProblem/Project/AutomationScript/PasswordGenerator/PasswordGenerator.py
@@ -5,3 +5,35 @@ Random password generate
 length user input
 save to file
 """
+
+def GeneratedPassword(length):
+    import random
+    import string
+
+    characters = string.ascii_letters + string.digits + string.punctuation      # ascii_letters: a-zA-Z, digits: 0-9, punctuation: special characters where space is not included
+    password = ''.join(random.choice(characters) for _ in range(length))        
+    return password
+
+def main():
+    try:
+        length = int(input("Enter the desired password length: "))
+        if length <= 0:
+            print("Please enter a positive integer for password length.")
+            return
+        
+    except ValueError:
+        print("Invalid input. Please enter a valid integer.")
+        return
+    
+    password = GeneratedPassword(length)
+    print(f"Generated Password: {password}")
+
+    try:
+        with open("PracticeProblem/Project/AutomationScript/PasswordGenerator/Passwords.txt", "a") as file:
+            file.write(password + "\n")
+    except Exception as e:
+        print(f"Error saving password to file: {e}")
+
+if __name__ == "__main__":
+    main()
+        
